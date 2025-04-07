d1baf21a54a0972ea1fbb8a3f460e024ec71900e
Kunal Rana <kunalranabsr9458@gmail.com>
2025-04-07 11:28:02 +0530

    basic setup done

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+require('dotenv').config();
+
+app.use(express.json())
+
+app.get('/health',(req,res) => {
+    res.status(200).json({status:"Server is Healthy"})
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,() => {
+    console.log(`Server is Listening at Port ${PORT}`)
+})
