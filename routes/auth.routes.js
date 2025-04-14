28c6efb4381fe28632e0b8ab4930915ab6571d1c
kunalrana9458 <kunalranabsr9458@gmail.com>
2025-04-14 15:16:43 +0530

    Backend validation to make our application more secure

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,8 +2,10 @@ const express = require('express')
 const {register,login} = require('../controllers/index')
 const router = express.Router()
 const {auth,isAdmin,isUser} = require("../middlewares/index")
+const validate = require('../middlewares/validate')
+const {registerSchema} = require('../validators/user.validator')
 
-router.post('/register',register)
+router.post('/register',validate(registerSchema),register)
 router.post('/login',login)
 router.post('/profile',auth,(req,res) => {
     console.log("This is protected route")
